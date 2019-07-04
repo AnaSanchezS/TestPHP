b958b6f5ca9666e867bcb84bf714fe342d536739
AnaSanchezS <33376532+AnaSanchezS@users.noreply.github.com>
2019-07-04 11:12:49 -0500

    refresh

--- Tests/HelloWorldTest.php
+++ Tests/HelloWorldTest.php
@@ -19,8 +19,29 @@ class HelloWorldTest extends PHPUnit_Framework_TestCase
         $this->pdo->query("DROP TABLE hello");
     }
 
-  
-  
+    public function testHelloWorld()
+    {
+        $helloWorld = new HelloWorld($this->pdo);
 
-    
+        $this->assertEquals('Hello World', $helloWorld->hello());
+    }
+
+    public function testHello()
+    {
+        $helloWorld = new HelloWorld($this->pdo);
+
+        $this->assertEquals('Hello Bar', $helloWorld->hello('Bar'));
+    }
+
+    public function testWhat()
+    {
+        $helloWorld = new HelloWorld($this->pdo);
+
+        $this->assertFalse($helloWorld->what());
+
+        $helloWorld->hello('Bar');
+
+        $this->assertEquals('Bar', $helloWorld->what());
+    }
 }
+
