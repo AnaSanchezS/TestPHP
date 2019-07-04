322a3598b651c63c29952fce205eed7fd35c6cf1
AnaSanchezS <33376532+AnaSanchezS@users.noreply.github.com>
2019-07-04 11:39:55 -0500

    correctignfile

--- Tests/HelloWorldTest.php
+++ Tests/HelloWorldTest.php
@@ -11,10 +11,23 @@ class HelloWorldTest extends PHPUnit_Framework_TestCase
     {
         $this->pdo = new PDO($GLOBALS['db_dsn'], $GLOBALS['db_username'], $GLOBALS['db_password']);
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $this->pdo->query("CREATE TABLE hello (what VARCHAR(50) NOT NULL)");
+        $this->pdo->query("CREATE TABLE login (user VARCHAR(50) NOT NULL)");
     }
 
-    public function tearDown()
+	public function tearDown()
+    {
+        $this->pdo->query("DROP TABLE hello");
+    }
+	
+	
+	function getuser($conn) {
+    $sql = 'SELECT * FROM login ORDER BY name';
+    foreach ($conn->query($sql) as $row) {
+        print $row['user'] . "\t";
+        //print $row['name'] . "\t";
+      }
+}
+      public function tearDown()
     {
         $this->pdo->query("DROP TABLE hello");
     }
